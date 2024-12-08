3a5f2658f9d5858978b95a90e11d4e63a1782d3d
Peter Rasmussen <pmrrasmussen@icloud.com>
2024-12-08 16:32:26 +0100

    Add if statements and pre- and infix operators

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -3,6 +3,7 @@ package evaluator
 import (
 	"monkey/ast"
 	"monkey/object"
+	"monkey/token"
 )
 
 var (
@@ -19,7 +20,20 @@ func Eval(node ast.Node) object.Object {
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 
-	// Expressions
+		// Expressions
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+
+	// Literals
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
@@ -29,6 +43,7 @@ func Eval(node ast.Node) object.Object {
 	return nil
 }
 
+// Evaluators
 func evalStatements(statements []ast.Statement) object.Object {
 	var result object.Object
 
@@ -39,10 +54,106 @@ func evalStatements(statements []ast.Statement) object.Object {
 	return result
 }
 
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case token.BANG:
+		return evalBangOperatorExpression(right)
+	case token.MINUS:
+		return evalMinusOperatorExpression(right)
+	default:
+		return nil
+	}
+}
+
+func evalInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left.(*object.Integer), right.(*object.Integer))
+	case operator == token.EQ:
+		return nativeBoolToBooleanObject(left == right)
+	case operator == token.NOT_EQ:
+		return nativeBoolToBooleanObject(left != right)
+	default:
+		return NULL
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+// Operator evaluation
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object {
+	value := right.(*object.Integer)
+	if value == nil {
+		return NULL
+	}
+
+	return &object.Integer{Value: -value.Value}
+}
+
+func evalIntegerInfixExpression(operator string, left *object.Integer, right *object.Integer) object.Object {
+	switch operator {
+	// Arithmetic
+	case token.PLUS:
+		return &object.Integer{Value: left.Value + right.Value}
+	case token.MINUS:
+		return &object.Integer{Value: left.Value - right.Value}
+	case token.ASTERISK:
+		return &object.Integer{Value: left.Value * right.Value}
+	case token.SLASH:
+		return &object.Integer{Value: left.Value / right.Value}
+
+	// Comparisons
+	case token.EQ:
+		return nativeBoolToBooleanObject(left.Value == right.Value)
+	case token.NOT_EQ:
+		return nativeBoolToBooleanObject(left.Value != right.Value)
+	case token.LT:
+		return nativeBoolToBooleanObject(left.Value < right.Value)
+	case token.GT:
+		return nativeBoolToBooleanObject(left.Value > right.Value)
+	default:
+		return NULL
+	}
+}
+
+// Helpers
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	if input {
 		return TRUE
 	} else {
 		return FALSE
+	}
+}
+
+func isTruthy(value object.Object) bool {
+	switch value {
+	case FALSE:
+		return false
+	case NULL:
+		return false
+	default:
+		return true
 	}
 }
--- monkey/object/object.go
+++ monkey/object/object.go
@@ -28,8 +28,8 @@ type Boolean struct {
 	Value bool
 }
 
-func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
-func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (b Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
 type Null struct{}
 
